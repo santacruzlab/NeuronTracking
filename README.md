54312a34c7cf37d1733c0528358ee3d26e61825d
Hung-Yun <hungyun.lu@utexas.edu>
2025-04-14 18:36:58 -0500

    Named trackMEA

---
+++ README.md
@@ -0,0 +1,4 @@
+# The tracking algorithm for traditional microelectrode arrays (trackMEA)
+
+## Setup
+`pip install -e src`
--- src/trackMEA/track.py
+++ src/trackMEA/track.py
@@ -7,9 +7,11 @@ Created on Wed Feb 19 13:39:59 2025
 """
 
 import os 
+import re
 import random
 import numpy as np
 import pandas as pd
+import datetime
 from collections import Counter, defaultdict
 
 
@@ -19,6 +21,25 @@ DATA_FOLDER = os.path.join(PROJECT_FOLDER, 'data')
 RESULT_FOLDER = os.path.join(PROJECT_FOLDER, 'result')
 DUMMY_NUMBER = 1e7 
 USEFUL_N_UNIT = 3
+
+pd.options.mode.chained_assignment = None  # Suppresses the warning
+
+
+def parse_unit(unit: str):
+    """
+    Parse the unit code into session, date, channel, and unit code.
+    
+    :param unit: The unit code in the format of 'date_channel_unit_code'.
+    :return: A tuple containing date, channel, and unit code.
+    """
+    pattern = r'([0-9]{8})_(([0-9]*)[a-d])' 
+
+    date = re.search(pattern, unit).group(1) # Like 20230101
+    unit_code = re.search(pattern, unit).group(2) # Like 22a
+    channel = re.search(pattern, unit).group(3) # Like 22
+    
+    return date, channel, unit_code
+
 
 class Tracking:
     
@@ -59,14 +80,17 @@ class Tracking:
         # Combine both data
         self.metadata['waveform'] = [self.waveforms[i] for i in range(len(self.metadata))]
 
-        # Extract channels
+        # Extract channels 
         self.channels = None
         self.find_channels()
 
         # Construct useful dataframes
         self.useful_df: pd.DataFrame = None
         self.useful_df = self.metadata[self.metadata['channel'].isin(self.channels)]
-        self.useful_df.loc[:, 'unit'] = self.useful_df['date'].astype(str) + "_" + self.useful_df['unit_code']
+        self.useful_df.at[:, 'unit'] = self.useful_df['date'].astype(str) + "_" + self.useful_df['unit_code'].astype(str)
+
+        # Extract dates
+        self.dates = self.useful_df.date.unique()
 
         # Calculate similarity
         print(f'[{self.subject}] Calculating similarities')
@@ -85,8 +109,8 @@ class Tracking:
         self.clusters: pd.DataFrame = None
         self.useful_clusters: pd.DataFrame = None
         self.tuning_params: dict = dict()
-        self.calc_matched_units()
-        self.get_clusters()
+        # self.calc_matched_units()
+        # self.get_clusters()
 
 
     def find_channels(self):
@@ -204,17 +228,24 @@ class Tracking:
                     self.threshold = 0.562
                     self.threshold_fussy = 0.008
                 case 'Mouse1':
-                    raise ValueError('Thresholds for Mouse1 are not computed yet.')
+                    self.threshold = 0.573
+                    self.threshold_fussy = 0.002
                 case 'Mouse2':
-                    raise ValueError('Thresholds for Mouse1 are not computed yet.')
+                    self.threshold = 0.636
+                    self.threshold_fussy = 0.004
                 case 'Mouse3':
-                    raise ValueError('Thresholds for Mouse1 are not computed yet.')
+                    self.threshold = 0.585
+                    self.threshold_fussy = 0.002
                 case 'Mouse4':
-                    raise ValueError('Thresholds for Mouse1 are not computed yet.')
+                    self.threshold = 0.602
+                    self.threshold_fussy = 0.002
                 case 'Mouse5':
-                    raise ValueError('Thresholds for Mouse1 are not computed yet.')
+                    self.threshold = 0.596
+                    self.threshold_fussy = 0.004
         else:
-            self.get_threshold()
+            self.get_threshold(95, False)
+            print(self.threshold)
+            print(self.threshold_fussy)
      
                 
     @staticmethod
@@ -298,14 +329,13 @@ class Tracking:
                 name = potential_match_name[k] # Shorthand
                 
                 # Grab the dates for all labels 
-                # TODO: fix parse_unit
-                counts = Counter([parse_unit(n)[1] for n in name]) 
+                counts = Counter([parse_unit(n)[0] for n in name], subject=self.subject) 
                 bug_dates = [key for key, value in counts.items() if value > 1]
                 
                 if len(bug_dates) > 0: # Then we will deal with this case
                 
                     # Find where in that potential match (var: name) has repetition
-                    ind_for_potential_match = find_repeated_indices([parse_unit(n)[0] for n in name])
+                    ind_for_potential_match = find_repeated_indices([parse_unit(n)[0] for n in name], subject=self.subject) 
                     
                     # Use a list to collect the indices to be removed
                     ind_to_remove = []
@@ -332,7 +362,7 @@ class Tracking:
         if verify:
             for i in range(len(self.matched_units)):
                 unit = self.matched_units[i]
-                unique_dates = np.unique([parse_unit(u)[1] for u in unit])
+                unique_dates = np.unique([parse_unit(u)[0] for u in unit], subject=self.subject) 
                 if len(unit) > len(unique_dates):
                     print(f'Matched units {i} has conflict units.')
                     print(f'Use self.matched_units[{i}] to debug.')
@@ -361,10 +391,8 @@ class Tracking:
             # Assign the ID to useful_df so that we can grab rotation angles and direct/indirect units.
             for u in range(len(cluster_df)):
                 unit = cluster_df['unit'].iloc[u]
-                row = np.where(self.useful_df.unit==unit)[0]                
-                self.useful_df.loc[row,'cluster_ID'] = cluster_ID 
-                cluster_df.loc[u, 'rotation'] = self.useful_df.loc[row[0],'rotation']
-                cluster_df.loc[u, 'is_direct'] = self.useful_df.loc[row[0],'is_direct']
+                row = np.where(self.useful_df.unit==unit)[0][0]             
+                self.useful_df.at[row,'cluster_ID'] = cluster_ID 
                 
         self.useful_clusters = self.clusters[self.clusters['n_unit'] >= USEFUL_N_UNIT].reset_index(drop=True)
                 
@@ -372,9 +400,6 @@ class Tracking:
     def get_threshold(self, pct: float, PLOT: bool):
         
         """
-
-        NOT DONE YET.
-
         Calculate the similarity threshold for determining matched units.
 
         This function generates a null distribution of similarity scores by randomly pairing 
@@ -399,6 +424,7 @@ class Tracking:
                 subj_ch_session[i,j] = len(self.useful_df[(self.useful_df.date==date) & 
                                                         (self.useful_df.channel==ch)])
         
+        # Store the channels and dates indicies.
         loc = np.zeros((len(self.dates), 2))
         loc[:,1] = np.arange(len(self.dates))
         
@@ -408,49 +434,54 @@ class Tracking:
             
             print(f'{it} iteration')
             run = 0
-            
             dist = []
-            n_unit = []
             
             while len(dist) < 5000:
                 
                 run += 1        
                 loc[:,0] = random.sample(range(len(self.channels)), len(self.dates))
                 
-                used_pair = []
+                used_pair = [] # The pairs of channels and dates to obtain the null.
                 for l in loc:
                     if subj_ch_session[int(l[0]), int(l[1])] != 0:
-                        used_pair.append((int(self.channels[int(l[0])]), 
-                                        self.dates[int(l[1])]))
+                        used_pair.append((int(self.channels[int(l[0])]), self.dates[int(l[1])]))
+                        
+                # used_pair should be greater than 2 (meaning at least 3), otherwise comparison would be meaningless
+                # If len(used_pair) == 1 -> Nothing to compare.
+                # If len(used_pair) == 2 -> Cannot be rescaled.
+                if len(used_pair) > 2:
+                    sim_temp = np.zeros((2, len(used_pair), len(used_pair)))
+                    for m, metric in enumerate(['correlation', 'euclidean']):
+                        for i, (ch1, date1) in enumerate(used_pair):
+                            for j, (ch2, date2) in enumerate(used_pair):
+                                
+                                # Obtain waveformS from designated channels and dates. 
+                                wf_s1 = self.useful_df[(self.useful_df['channel']==ch1)&(self.useful_df['date']==date1)]['waveform']
+                                wf_s2 = self.useful_df[(self.useful_df['channel']==ch2)&(self.useful_df['date']==date2)]['waveform']
+                                
+                                # Randomly pick one if > 1 unit in that channel.
+                                wf1 = wf_s1.iloc[random.randint(0, len(wf_s1)-1) if len(wf_s1) > 1 else 0].flatten()
+                                wf2 = wf_s2.iloc[random.randint(0, len(wf_s2)-1) if len(wf_s2) > 1 else 0].flatten()
+                                
+                                sim_temp[m,i,j] = self.similarity(wf1, wf2, metric)
+
+                        # Rescale based on each metric. Note that if len(used_pair) < 3, then it cannot be rescaled properly.
+                        sim_temp[m] = self.rescale(sim_temp[m], metric=metric)
+
+                    # Obtain total similarity matrix for this batch.
+                    total_temp = sim_temp.mean(axis=0)
                 
-                sim_temp = np.zeros((2, len(used_pair), len(used_pair)))
-                for m, metric in enumerate(['correlation', 'euclidean']):
-                    for i, (ch1, date1) in enumerate(used_pair):
-                        for j, (ch2, date2) in enumerate(used_pair):
-                            
-                            wf_s1 = self.useful_df[(self.useful_df['channel']==ch1)&(self.useful_df['date']==date1)]['waveform']
-                            wf_s2 = self.useful_df[(self.useful_df['channel']==ch2)&(self.useful_df['date']==date2)]['waveform']
-                            
-                            # Randomly pick one if > 1 unit in that channel.
-                            wf1 = wf_s1.iloc[random.randint(0, len(wf_s1)-1) if len(wf_s1) > 1 else 0]
-                            wf2 = wf_s2.iloc[random.randint(0, len(wf_s2)-1) if len(wf_s2) > 1 else 0]
-                            
-                            sim_temp[m,i,j] = self.similarity(wf1, wf2, metric)
-                    sim_temp[m] = self.rescale(sim_temp[m], metric=metric)
-                total_temp = sim_temp.mean(axis=0)
-            
-                dist += [float(total_temp[i, j]) 
-                        for i in range(len(total_temp)) 
-                        for j in range(i + 1, len(total_temp))]
-                n_unit.append(len(total_temp))
-            
+                    # Obtain the lower triangle of the total similarity.
+                    dist += [float(total_temp[i, j]) 
+                            for i in range(len(total_temp)) 
+                            for j in range(i + 1, len(total_temp))]
+                
             thres = np.percentile(dist, pct)                
             threshold.append(thres)
 
         self.threshold = np.mean(threshold)
         self.threshold_fussy = np.std(threshold)
         
- 
         
 class Matched:
     
@@ -486,20 +517,17 @@ class Matched:
     def make_df(self):  
         
         # The first part is more general information from the unit ID.
-        session = []
         date = []
         unit_code = []
         channel = []
         for i in range(self.n_unit):
-            s, d, c, u = parse_unit(self.units[i])
-            session.append(s)
+            d, c, u = parse_unit(self.units[i])
             date.append(d)
             channel.append(c)
             unit_code.append(u)
             
         self.df = pd.DataFrame({
             'unit': self.units,
-            'session': session,
             'date': date,
             'channel': channel,
             'unit_code': unit_code,
@@ -522,6 +550,7 @@ class Matched:
 def main():
     print('Tracking algorithm is running.')
     a: Tracking = Tracking('airp')
+
 
 if __name__ == '__main__':
     main()
