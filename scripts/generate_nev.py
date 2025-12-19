9bbf560b5eafa6cf1b8692c867b22885bb03fb74
cole barnett <colebarnett@utexas.edu>
2025-12-19 14:39:45 -0600

    adding a script to generate nev output pkl files for all braz and airp data

---
+++ scripts/generate_nev.py
@@ -0,0 +1,6 @@
+from preprocess_santacruz_data import _generate_nev_output
+session = 'airp20211202_04_te1598'
+_generate_nev_output([session], count=False)
+
+# airport 64 electrodes
+# brazos 128 electrodes
