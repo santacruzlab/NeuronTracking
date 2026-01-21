00925542176e1c36768e38796814399f8312b1dc
cole barnett <colebarnett@utexas.edu>
2026-01-21 16:21:30 -0600

    runs preprocess_BMI using pkl input folder and saves into nev_label folder.

---
+++ scripts/generate_waveforms.py
@@ -0,0 +1,7 @@
+from preprocess_santacruz_data import preprocess_BMI
+
+
+
+preprocess_BMI( 
+    input_folder = r"F:/cole/neuron_tracking_nev_outputs/neuron_tracking_pkl_files", 
+    output_folder = r"F:/cole/neuron_tracking_nev_outputs/neuron_tracking_nev_labels")
